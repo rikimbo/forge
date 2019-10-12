66669c2f03eb1866a63032e208b08b5456bb46f8
Eric <rikimbo@gmail.com>
2019-10-12 10:34:49 -0700

    Remove debug messages from multiplayer code.

--- forge-gui/src/main/java/forge/gamemodes/net/client/FGameClient.java
+++ forge-gui/src/main/java/forge/gamemodes/net/client/FGameClient.java
@@ -82,7 +82,7 @@ public class FGameClient implements IToServer {
 
     @Override
     public void send(final NetEvent event) {
-        System.out.println("Client sent " + event);
+        //System.out.println("Client sent " + event);
         channel.writeAndFlush(event);
     }
 
